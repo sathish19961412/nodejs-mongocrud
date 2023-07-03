a1979913d5909d05c482ed095e280f2092546c28
sathish19961412 <sathishfreelanc5@gmail.com>
2023-07-03 16:24:41 +0530

    testing to form data

---
+++ app.js
@@ -0,0 +1,42 @@
+const express=require('express');
+const app=express();
+const bodyparser=require('body-parser');
+const exhbs=require('express-handlebars');
+const dbo=require('./db');
+
+app.engine('hbs',exhbs.engine({layoutsDir:'views/',defaultLayout:'main',extname:'hbs'}))
+app.set('view engine','hbs');
+app.set('views','views');
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.get('/',async (req,res)=>{
+
+   let database= await dbo.getDatabase();
+   const collection=database.collection('books');
+   const cursor=collection.find({})
+   let emploees= await cursor.toArray();
+
+   let message=''; 
+
+   switch (req.query.status){
+         case '1':
+            message='Inserted Successfully';
+            break;
+         
+         default:
+            break; 
+   }
+   res.render('main',{message,emploees})
+})
+
+app.post('/store_book',async (req,res)=>{
+
+   let database= await dbo.getDatabase();
+   const collection=database.collection('books');
+   let book={title:req.body.title,author:req.body.author}
+   await collection.insertOne(book);
+   return res.redirect('/?status=1');
+})
+
+app.listen(8000,()=>{console.log('Listening to 8000 Port');})
